70b39608f17db727f51792c672a1b2eaa4203c6b
Pieter Kok <pieterk@bbd.co.za>
2022-05-13 16:49:59 +0200

    Add APIs for back-end.

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,50 @@ app.post("/login", (req, res) => {
     res.status(418);
 });
 
-app.get("/matches", (req, res) => {
+app.get("/matches/:userId", (req, res) => {
+    let userId = req.params.userId;
+
+    res.status(418);
+});
+
+app.get("/userProfileDetails/:userId", (req, res) => {
+    let userId = req.params.userId;
+
+    res.status(418);
+});
+
+app.post("/userProfileDetails/:userId", (req, res) => {
+    let userId = req.params.userId;
+    let details = req.body;
+
+    res.status(418);
+});
+
+app.patch("/userProfileDetails/:userId", (req, res) => {
+    let userId = req.params.userId;
+    let details = req.body;
+
+    res.status(418);
+});
+
+app.get("/chats/:friendId", (req, res) => {
+    let fiendId = req.params.userId;
+    let {userId}
+
+    res.status(418);
+});
+
+app.post("/like/:friendId", (req, res) => {
+    let friendId = req.params.friendId;
+    let { userId } = req.body;
+
+    res.status(418);
+});
+
+app.post("/message/:recipientId", (req, res) => {
+    let recipientId = req.params.userId;
+    let { senderId, message } = req.body;
+
     res.status(418);
 });
 
